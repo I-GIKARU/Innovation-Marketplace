52a9d18a2170eb130989fe6bfe5874bb23bf4830
Elizabeth Njuguna <elizabethnjuguna217@gmail.com>
2025-07-16 12:35:51 +0300

    addsclient  model

--- server/models/projects.py
+++ server/models/projects.py
@@ -46,3 +46,24 @@ class Admin(db.Model, SerializerMixin):
         
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
+
+class Client(db.Model, SerializerMixin):
+    __tablename__ = 'clients'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    company = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+    
+    interests = db.relationship('ClientInterest', back_populates='client')
+    orders = db.relationship('Order', back_populates='client')
+    
+    serialize_rules = ('-password_hash', '-interests.client', '-orders.client',)
+    
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
