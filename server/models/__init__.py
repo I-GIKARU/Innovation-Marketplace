86aebdd40691c6853fa2fb7345a196f25801547b
Enock Tangus <tanguskipngeno@gmail.com>
2025-07-16 19:53:42 +0300

    make models as package

---
+++ server/models/__init__.py
@@ -0,0 +1,14 @@
+from .merchandise import User, Merchandise, OrderItem, Order, Payment
+from .projects import Student, Admin, Client, Category, Project, ProjectStudent, ClientInterest, Review
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+__all__ = [
+    'db', 'bcrypt',
+    'User', 'Merchandise', 'OrderItem', 'Order', 'Payment',
+    'Student', 'Admin', 'Client', 'Category', 'Project', 
+    'ProjectStudent', 'ClientInterest', 'Review'
+]
