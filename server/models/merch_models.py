2b7df01972d8ce54affa5b321072f3d41826c364
Elizabeth Njuguna <elizabethnjuguna217@gmail.com>
2025-07-16 11:27:24 +0300

    adds merch users model

---
+++ server/models/merch_models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    
+    serialize_rules = ('-password_hash',)  # Exclude password from serialization
+    
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
