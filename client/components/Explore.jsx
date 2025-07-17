91c89114e538eab8b591352938addc6d8cf02db6
monicah-monic <monicahwanjiru205@gmail.com>
2025-07-17 07:08:15 +0300

    modified explore

--- client/components/Explore.jsx
+++ client/components/Explore.jsx
@@ -10,16 +10,28 @@ const Explore = () => {
         <div className="flex justify-center">
           <Image src="/images/explore.jpg" alt="Explore Visual"width={500} height={400}className="rounded-lg shadow-xl"/>
         </div>
+        
 
-        <div className="bg-gray-100 rounded-2xl shadow-lg p-6 flex flex-col items-center">
-          <p className="text-gray-800 text-lg mb-4 text-center">
-            Discover how we solve real world problems through code.From secure APIs to scalable dbs, beautifuly made user Interfaces, discover all here.
-          </p>
-          <Image
-            src="/images/exlore.jpg" alt="people discusing project"width={400} height={300} className="rounded-lg mb-4" />
-          <button className="mt-auto px-6 py-2 bg-orange-400 text-white rounded-full hover:bg-orange-600 transition">
-            Explore Now
+         
+        <div className="flex flex-col gap-6">
+          <div className="border border-orange-200 rounded-2xl shadow-md p-10 flex flex-col justify-between h-[300px]">
+            <p className="text-gray-800 text-md text-center">
+              Every year, Moringa students create brilliant software projects — built with real problems in mind.
+              But after demo day, those projects often disappear.
+               No users, no feedback, no income, no job offers.
+             The result? Missed chances for innovation, growth, and impact. We’re here to change that.
+            </p>
+            <button className="bg-orange-400 hover:bg-orange-500 text-white px-6 py-3 rounded-full font-medium">
+            Explore projects
           </button>
+          </div>
+
+         
+          <div className="flex justify-center">
+            <Image
+              src="/images/exlore.jpg" alt="people discussing project" width={600} height={300} className="rounded-lg shadow"/>
+          </div>
+
         </div>
       </div>
     </section>
