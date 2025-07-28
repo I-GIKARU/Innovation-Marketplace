def6efdd7846b6a5cbd08e27cea567e1a1141edb
I-GIKARU <aizohgikaru@gmail.com>
2025-07-28 06:17:38 +0300

    update client

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -40,7 +40,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
+        destination: 'https://innovation-marketplace-840370620772.us-central1.run.app/api/:path*',
       },
     ];
   },
